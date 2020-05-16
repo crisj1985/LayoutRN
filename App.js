ff2d12e4ffecf6106968e9642ee75b423bfb58a0
crisj1985 <crisj1985@hotmail.com>
2020-05-15 23:07:50 -0500

    ScrollView Horizontal

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 const App = () => {
     return (
       <>
-        <View style={{flexDirection:'row'}}>
-          <Image source={require('./assets/img/bg.jpg')} style={styles.banner} />
+        <View>
+          <View style={{ flexDirection: 'row' }}>
+            <Image source={require('./assets/img/bg.jpg')} style={styles.banner} />
+          </View>
+          <View style={styles.contenedor}>
+            <Text style={styles.titulo}>Que haces en Paris</Text>
+            <ScrollView
+                horizontal
+            >
+              <View>
+                <Image source={require('./assets/img/actividad1.jpg')} style={styles.ciudad} />
+              </View>
+              <View>
+                <Image source={require('./assets/img/actividad2.jpg')} style={styles.ciudad} />
+              </View>
+              <View>
+                <Image source={require('./assets/img/actividad3.jpg')} style={styles.ciudad} />
+              </View>
+              <View>
+                <Image source={require('./assets/img/actividad4.jpg')} style={styles.ciudad} />
+              </View>
+              <View>
+                <Image source={require('./assets/img/actividad5.jpg')} style={styles.ciudad} />
+              </View>
+            </ScrollView>
+          </View>
         </View>
       </>
     )
@@ -13,10 +37,22 @@ const App = () => {
 
 const styles = StyleSheet.create({
   banner: {
-    height:250,
-    flex:1
+    height: 200,
+    flex: 1,
   },
- 
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginVertical: 20,
+  },
+  contenedor: {
+    marginHorizontal: 10,
+  },
+  ciudad: {
+    width:200,
+    height:250,
+    marginRight:10
+  },
 })
 
 export default App
