c4ec92e378e8b3ed91cbdf2d23f3234f43eec499
crisj1985 <crisj1985@hotmail.com>
2020-05-15 23:15:27 -0500

    ScrollView Vertical, alojamientos

--- App.js
+++ App.js
@@ -4,15 +4,13 @@ import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 const App = () => {
     return (
       <>
-        <View>
+        <ScrollView>
           <View style={{ flexDirection: 'row' }}>
             <Image source={require('./assets/img/bg.jpg')} style={styles.banner} />
           </View>
           <View style={styles.contenedor}>
             <Text style={styles.titulo}>Que haces en Paris</Text>
-            <ScrollView
-                horizontal
-            >
+            <ScrollView horizontal>
               <View>
                 <Image source={require('./assets/img/actividad1.jpg')} style={styles.ciudad} />
               </View>
@@ -29,30 +27,51 @@ const App = () => {
                 <Image source={require('./assets/img/actividad5.jpg')} style={styles.ciudad} />
               </View>
             </ScrollView>
+            <Text style={styles.titulo}>Los mejores alojamientos</Text>
+            <View>
+              <View style={{ flexDirection: 'row' }}>
+                <Image source={require('./assets/img/mejores1.jpg')} style={styles.mejores} />
+              </View>
+            </View>
+            <View>
+              <View style={{ flexDirection: 'row' }}>
+                <Image source={require('./assets/img/mejores2.jpg')} style={styles.mejores} />
+              </View>
+            </View>
+            <View>
+              <View style={{ flexDirection: 'row' }}>
+                <Image source={require('./assets/img/mejores3.jpg')} style={styles.mejores} />
+              </View>
+            </View>
           </View>
-        </View>
+        </ScrollView>
       </>
     )
 };
 
 const styles = StyleSheet.create({
   banner: {
-    height: 200,
+    height: 175,
     flex: 1,
   },
   titulo: {
     fontWeight: 'bold',
     fontSize: 24,
-    marginVertical: 20,
+    marginVertical: 15,
   },
   contenedor: {
     marginHorizontal: 10,
   },
   ciudad: {
-    width:200,
-    height:250,
-    marginRight:10
+    width: 200,
+    height: 200,
+    marginRight: 10,
   },
+  mejores:{
+      width:"100%",
+      height:150,
+      marginVertical:5
+  }
 })
 
 export default App
